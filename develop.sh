142497798d74885d2e1a96db7816829c412370e2
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-10-02 12:18:51 -0600

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -1,2 +1,8 @@
+cd $GOPATH/src/github.com/SkyrisBactera/StudentVIEW
 gopherjs build -w github.com/SkyrisBactera/StudentVIEW &
 gopherjs serve --localmap ../ &
+while [ true ]
+do
+    git pull    
+    sleep 60
+done
